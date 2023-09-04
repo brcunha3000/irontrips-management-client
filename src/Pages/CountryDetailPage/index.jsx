035b0ea77376424a8ad36bc7c9d2c4677829389e
Bruno Cunha <brunorcunha3000@gmail.com>
2023-09-04 09:59:02 +0100

    country details changes

--- src/Pages/CountryDetailPage/index.jsx
+++ src/Pages/CountryDetailPage/index.jsx
@@ -1,10 +1,40 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
 
-function CountryDetailsPage() {  
-  
+const API_URL = "http://localhost:5005";
+
+function CountryDetailsPage() {
+  const [country, setCountry] = useState(null);
+  const [fetching, setFetching] = useState(true);
+  const { countryCode } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/theglobe/${countryCode}`)
+      .then((response) => {
+        const oneCountry = response.data;
+        setCountry(oneCountry);
+        setFetching(false);
+      })
+      .catch((error) => console.log(error));
+  }, [countryCode, setCountry]);
+
   return (
-  <div><p>Country Detail</p></div>
-  )
+    <div>
+      {fetching && <p>Loading...</p>}
+      {country && (
+        <div>
+          <h1>{country.name.common}</h1>
+          <p>Favorites: {country.favorites}</p>
+          <p>Capital: {country.capital}</p>
+          <img src={country.flags.png} alt={country.name.common} />
+          <a href={country.maps.googleMaps}>Maps</a>
+        </div>
+      )}
+      <Link to="/theglobe">Back to The Globe</Link>
+    </div>
+  );
 }
 
 export default CountryDetailsPage;
